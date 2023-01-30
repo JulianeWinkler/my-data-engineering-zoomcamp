be2ffeceef75ebef49c04477dff728c38ad5b952
Juliane <juliane.winkler90@googlemail.com>
2023-01-30 18:51:44 +0100

    added first prefect flow in upload_data script

--- week_2/upload_data.py
+++ week_2/upload_data.py
@@ -3,15 +3,10 @@ import pandas as pd
 from sqlalchemy import create_engine
 from time import time
 import argparse
+from prefect import flow, task
 
-def main(params):
-   user = params.user
-   password = params.password
-   host = params.host 
-   port = params.port 
-   db = params.db
-   table_name = params.table_name
-   url = params.url
+@task(log_prints=True, retries=2)
+def ingest_data(user, password, host, port, db, table_name, url):
    
    if url.endswith('.csv.gz'):
       csv_name = 'output.csv.gz'
@@ -26,8 +21,8 @@ def main(params):
    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
    df = next(df_iter)
    if 'tripdata' in url:
-      df['lpep_pickup_datetime'] = pd.to_datetime(df.lpep_pickup_datetime)
-      df['lpep_dropoff_datetime'] = pd.to_datetime(df.lpep_dropoff_datetime)
+      df['tpep_pickup_datetime'] = pd.to_datetime(df.tpep_pickup_datetime)
+      df['tpep_dropoff_datetime'] = pd.to_datetime(df.tpep_dropoff_datetime)
    else:
       print('no transformations necessary')
 
@@ -40,8 +35,8 @@ def main(params):
 
          df = next(df_iter)
          if 'tripdata' in url:
-            df['lpep_pickup_datetime'] = pd.to_datetime(df.lpep_pickup_datetime)
-            df['lpep_dropoff_datetime'] = pd.to_datetime(df.lpep_dropoff_datetime)
+            df['tpep_pickup_datetime'] = pd.to_datetime(df.tpep_pickup_datetime)
+            df['tpep_dropoff_datetime'] = pd.to_datetime(df.tpep_dropoff_datetime)
          else:
             print('no transformations necessary')
               
@@ -54,20 +49,18 @@ def main(params):
          print('Finished ingesting data into the postgres database')
          break
 
+@flow(name="ingest_flow")
+def main_flow():
+   user = "root"
+   password = "root"
+   host = "localhost"
+   port = "5432"
+   db = "ny_taxi"
+   table_name = "yellow_taxi_trips"
+   csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+
+   ingest_data(user, password, host, port, db, table_name, csv_url)
+
 
 if __name__ == '__main__':
-
-   parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
-
-   parser.add_argument('--user', required=True, help='user name for postgres')
-   parser.add_argument('--password', required=True, help='password for postgres')
-   parser.add_argument('--host', required=True, help='host for postgres')
-   parser.add_argument('--port', required=True, help='port for postgres')
-   parser.add_argument('--db', required=True, help='database name for postgres')
-   parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-   parser.add_argument('--url', required=True, help='url of the csv file')
-
-
-   args = parser.parse_args()
-
-   main(args)
+   main_flow()
