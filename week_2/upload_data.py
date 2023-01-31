9aba6d2c6d6cb7d19ab6cd5dfc894683cf48b1e2
Juliane <juliane.winkler90@googlemail.com>
2023-01-31 20:37:15 +0100

    added subtasks and connection blocks in prefect flow

--- week_2/upload_data.py
+++ week_2/upload_data.py
@@ -2,12 +2,16 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 from time import time
+from datetime import timedelta
 import argparse
 from prefect import flow, task
+from prefect.tasks import task_input_hash
+from prefect_sqlalchemy import SqlAlchemyConnector
 
-@task(log_prints=True, retries=2)
-def ingest_data(user, password, host, port, db, table_name, url):
-   
+
+@task(log_prints=True, retries=2, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def extract_url(url):
+
    if url.endswith('.csv.gz'):
       csv_name = 'output.csv.gz'
    else:
@@ -15,52 +19,50 @@ def ingest_data(user, password, host, port, db, table_name, url):
    
    os.system(f"wget {url} -O {csv_name}")
 
-   engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-
-   
    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
    df = next(df_iter)
+
    if 'tripdata' in url:
       df['tpep_pickup_datetime'] = pd.to_datetime(df.tpep_pickup_datetime)
       df['tpep_dropoff_datetime'] = pd.to_datetime(df.tpep_dropoff_datetime)
    else:
       print('no transformations necessary')
+   
+   return df
 
-   df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-   df.to_sql(name=table_name, con=engine, if_exists='append')
+@task(log_prints=True)
+def transform_data(df):
 
-   while True:
-      try:
-         t_start = time()
+   print(f"pre: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+   df = df[df['passenger_count'] != 0]
+   print(f"post: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+   
+   return df
 
-         df = next(df_iter)
-         if 'tripdata' in url:
-            df['tpep_pickup_datetime'] = pd.to_datetime(df.tpep_pickup_datetime)
-            df['tpep_dropoff_datetime'] = pd.to_datetime(df.tpep_dropoff_datetime)
-         else:
-            print('no transformations necessary')
-              
-         df.to_sql(name=table_name, con=engine, if_exists='append')
 
-         t_end = time()
-         
-         print('uploaded another chunk, took %.3f seconds' %(t_end - t_start))
-      except StopIteration:
-         print('Finished ingesting data into the postgres database')
-         break
+@task(log_prints=True, retries=3)
+def load_data(table_name, df):
+   connection_block = SqlAlchemyConnector.load("postgres-connector")
+
+   with connection_block.get_connection(begin=False) as engine:
+      df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+      df.to_sql(name=table_name, con=engine, if_exists='append')
+
+
+@flow(name="Subflow", log_prints=True)
+def log_subflow(table_name: str):
+    print(f"Logging Subflow for: {table_name}")
+
 
 @flow(name="ingest_flow")
-def main_flow():
-   user = "root"
-   password = "root"
-   host = "localhost"
-   port = "5432"
-   db = "ny_taxi"
-   table_name = "yellow_taxi_trips"
+def main_flow(table_name : str):
    csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
 
-   ingest_data(user, password, host, port, db, table_name, csv_url)
+   log_subflow(table_name)
+   raw_data = extract_url(csv_url)
+   data = transform_data(raw_data)
+   load_data(table_name, data)
 
 
 if __name__ == '__main__':
-   main_flow()
+   main_flow("yellow_taxi_trips")
