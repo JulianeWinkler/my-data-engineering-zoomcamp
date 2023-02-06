ac5f559fbd4528d43b70e2e99e63011ce15a576e
Juliane <juliane.winkler90@googlemail.com>
2023-02-06 18:27:04 +0100

    new script

--- homework_week_2/etl_web_to_gcs_parameterized.py
+++ homework_week_2/etl_web_to_gcs_parameterized.py
@@ -32,7 +32,7 @@ def clean(df : pd.DataFrame, color : str) -> pd.DataFrame:
 @task()
 def write_local(df : pd.DataFrame, color : str, dataset_file : str) -> Path:
     """write dataframe out locally as a parquet file"""
-    path = Path(f"data/{color}/{dataset_file}.parquet")
+    path = Path(f"../data/{color}/{dataset_file}.parquet")
     df.to_parquet(path, compression="gzip")
     return path
 
