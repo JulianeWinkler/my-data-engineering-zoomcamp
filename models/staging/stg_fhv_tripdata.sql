16713931eb18e80a39f8882dc64bfc032ac06c58
Juliane Winkler <juliane.winkler90@googlemail.com>
2023-02-23 13:59:43 +0000

    added fhv fact trips

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -4,13 +4,12 @@ WITH tripdata AS
 (
   SELECT 
   *
-  --,ROW_NUMBER() OVER(PARTITION BY (cast(vendorid as integer)), pickup_datetime) AS rn
   FROM{{ source('staging','fhv_nonpartitioned_tripdata') }}
-  --WHERE vendorid IS NOT NULL 
 )
 
 SELECT
-
+    -- identifiers 
+    {{dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime'])}} AS tripid,
     CAST(dispatching_base_num AS string) AS dispatching_base_num,
     CAST(Affiliated_base_number AS string) AS Affiliated_base_number,
     {{ dbt_utils.safe_cast('PUlocationid',  api.Column.translate_type("integer"))}} AS  pickup_locationid,
@@ -22,7 +21,6 @@ SELECT
     {{ dbt_utils.safe_cast('SR_Flag',  api.Column.translate_type("integer"))}} AS SR_Flag
 
 FROM tripdata
---WHERE rn = 1
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
