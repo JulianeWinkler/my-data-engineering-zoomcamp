e9503514556a80b5660b7591ac7e6f1e12129362
Juliane <juliane.winkler90@googlemail.com>
2023-02-27 18:19:04 +0000

    added functions to pyspark file

--- week_5/04_pyspark.py
+++ week_5/04_pyspark.py
@@ -1,19 +1,58 @@
 import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql import types
+from pyspark.sql import functions as F
 import pandas as pd
 
 spark = SparkSession.builder \
     .master("local[*]") \
     .appName('test') \
     .getOrCreate()
-
+"""
 #df = spark.read \
-    #.option("header", "true") \
-    #.csv("fhvhv_tripdata_2021-01.csv.gz")
+    .option("header", "true") \
+    .csv("fhvhv_tripdata_2021-01.csv.gz")
 
 #print(df.head(5))
 
-df_2 = pd.read_csv("fhvhv_tripdata_2021-01.csv.gz")
-print(df_2.head(5))
+schema = types.StructType([
+    types.StructField('hvfhs_license_num', types.StringType(), True),
+    types.StructField('dispatching_base_num', types.StringType(), True),
+    types.StructField('pickup_datetime', types.TimestampType(), True),
+    types.StructField('dropoff_datetime', types.TimestampType(), True),
+    types.StructField('PULocationID', types.IntegerType(), True),
+    types.StructField('DOLocationID', types.IntegerType(), True),
+    types.StructField('SR_Flag', types.StringType(), True)
+])
 
-spark.createDataFrame(df_2).show()
+
+#df = spark.read \
+    .option("header", "true") \
+    .schema(schema) \
+    .csv("fhvhv_tripdata_2021-01.csv.gz")
+
+"""
+
+#print(df.head(5))
+#df = df.repartition(24)
+#df.write.parquet('fhvhv/2021/01/')
+
+df = spark.read.parquet('fhvhv/2021/01/')
+print(df)
+df.printSchema()
+df \
+    .select('hvfhs_license_num','pickup_datetime','dropoff_datetime','PULocationID','DOLocationID') \
+    .filter(df.hvfhs_license_num == 'HV0003') \
+    .show()
+
+#just like in python you can define your own functions
+
+def crazy_stuff()
+
+#pyspark integrated sql functions
+
+df \
+    .withColumn('pickup_date',F.to_date(df.pickup_datetime)) \
+    .withColumn('dropoff_date',F.to_date(df.dropoff_datetime)) \
+    .show()
+
